cc2d7ffddc5f06515043b6fe1907801469ca17f6
Hyrkas <Miika.Hyrkas@student.oulu.fi>
2024-10-29 16:17:18 +0200

    US10 - Test 1

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -179,3 +179,62 @@ class TestBowlingGame(unittest.TestCase):
         game.set_first_bonus_throw(7)
 
         self.assertEqual(90, game.calculate_score())
+
+    def test_calculate_score_with_last_frame_strike(self):
+        game = BowlingGame()
+
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+
+        frame = Frame(10, 0)
+        game.add_frame(frame)
+        game.set_first_bonus_throw(7)
+        game.set_second_bonus_throw(2)
+
+        self.assertEqual(92, game.calculate_score())
+
+    def test_calculate_score_with_last_two_frames_strike(self):
+        game = BowlingGame()
+
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(10, 0))
+
+        frame = Frame(10, 0)
+        game.add_frame(frame)
+        game.set_first_bonus_throw(7)
+        game.set_second_bonus_throw(2)
+
+        self.assertEqual(110, game.calculate_score())
+
+    def test_calculate_perfect_game(self):
+        game = BowlingGame()
+
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+
+        game.set_first_bonus_throw(10)
+        game.set_second_bonus_throw(10)
+
+        self.assertEqual(300, game.calculate_score())
